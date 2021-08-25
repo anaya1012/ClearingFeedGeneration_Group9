7ffbea43459db2a944e9bf1cebed29a4291c70e8
Anaya Garde <56402674+anayagarde@users.noreply.github.com>
2021-08-25 23:53:32 +0530

    Update UploadDownloadController.java

--- Backend/ClearingFeedGeneration/src/main/java/com/controllers/UploadDownloadController.java
+++ Backend/ClearingFeedGeneration/src/main/java/com/controllers/UploadDownloadController.java
@@ -3,6 +3,7 @@ package com.controllers;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -21,7 +22,7 @@ import com.message.ResponseMessage;
 import com.model.FileInfo;
 import com.service.FileStorageService;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class UploadDownloadController {
 	@Autowired
